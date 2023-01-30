69ae0ba1cde8a8fc2c792a6bbcdd7b25b80b9ef4
fandi-adhitya <fandiadhitya96@gmail.com>
2023-01-30 23:39:09 +0700

    initial commit

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem({
+    required this.id, 
+    required this.title, 
+    required this.imageUrl
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        title : Text(
+          title,
+          textAlign: TextAlign.center,
+        )
+      ),   
+    ); 
+  }
+}
